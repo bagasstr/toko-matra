dba6f664d37a19f98ba0d1b8ce98789080184c26
bagasstr <bagassatrio2805@gmail.com>
2025-05-17 12:27:45 +0700

    first commit

--- src/components/ui/ThemesProvider.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { ThemeProvider, type ThemeProviderProps } from 'next-themes'
-export default function ThemesProvider({
-  children,
-  ...props
-}: ThemeProviderProps) {
-  return <ThemeProvider {...props}>{children}</ThemeProvider>
-}
