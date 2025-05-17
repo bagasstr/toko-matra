99b05d5f0d48c4a69c860fe4471871776f158411
bagasstr <bagassatrio2805@gmail.com>
2025-05-17 12:59:51 +0700

    first commit

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,3 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
-
-export const prisma = () => {
-  if (!globalForPrisma.prisma) {
-    globalForPrisma.prisma = new PrismaClient()
-  }
-  return globalForPrisma.prisma
-}
+export const prisma = new PrismaClient()
