5bd5b71aad9e72f1a32479954b4566e02cfe8cac
bagasstr <bagassatrio2805@gmail.com>
2025-05-17 12:36:39 +0700

    first commit

--- src/app/api/getSession/route.ts
+++
@@ -1,6 +0,0 @@
-import { validateSession } from '@/app/actions/session'
-
-export const GET = async () => {
-  const getSession = await validateSession()
-  return new Response(JSON.stringify(getSession), { status: 200 })
-}
