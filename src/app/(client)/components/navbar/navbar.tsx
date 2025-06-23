21092c530a6ff2a95c6a5b8b9747862fbf935535
bagasstr <bagassatrio2805@gmail.com>
2025-06-23 14:44:56 +0700

    optimze page

--- src/app/(client)/components/navbar/navbar.tsx
+++ src/app/(client)/components/navbar/navbar.tsx
@@ -1,11 +1,15 @@
 import MobileNavbar from './mobileNavbar'
 import DesktopNavbar from './desktopNavbar'
+import { validateSession } from '@/app/actions/session'
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await validateSession()
+  const userId = session?.user?.profile.id.toLowerCase()
+
   return (
     <header className='fixed top-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-sm'>
-      <MobileNavbar />
-      <DesktopNavbar />
+      <MobileNavbar userId={userId} />
+      <DesktopNavbar userId={userId} />
     </header>
   )
 }
